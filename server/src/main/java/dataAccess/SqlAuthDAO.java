60e039708343a8c2da1e5f8006faeab61af5a85f
creedthomp <ctt12@byu.edu>
2024-03-07 09:37:07 -0700

    Phase 4 push 2. auth sql

--- server/src/main/java/dataAccess/SqlAuthDAO.java
+++ server/src/main/java/dataAccess/SqlAuthDAO.java
@@ -1,4 +1,141 @@
 package dataAccess;
 
-public class SqlAuthDAO implements AuthDAO{
+import models.AuthTokenInformation;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashSet;
+
+public class SqlAuthDAO implements AuthDAO {
+
+    public SqlAuthDAO() throws DataAccessException {
+        configureDatabase();
+    }
+
+    public boolean findAuth(String token) throws DataAccessException {
+        try (var connection = DatabaseManager.getConnection()) {
+            var statement = "SELECT authtoken FROM auth WHERE authtoken = ?";
+            try (var ps = connection.prepareStatement(statement)) {
+                ps.setString(1, token);
+                try (var rs = ps.executeQuery()) {
+                    return rs.next();
+                }
+            }
+        }
+        catch (SQLException exception) {
+            throw new DataAccessException("Error unauthorized");
+        }
+    }
+
+    public HashSet<AuthTokenInformation> getAuthList() throws DataAccessException {
+        HashSet<AuthTokenInformation> authList = new HashSet<>();
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT username, authtoken FROM auth";
+            try (var ps = conn.prepareStatement(statement)) {
+                try (var rs = ps.executeQuery()) {
+                    while (rs.next()) {
+                        authList.add(readAuthList(rs));
+                    }
+                }
+            }
+        }
+        catch (Exception e) {
+            throw new DataAccessException("Error: unable to read data");
+        }
+        return authList;
+    }
+
+    private AuthTokenInformation readAuthList(ResultSet rs) throws SQLException {
+        var authtoken = rs.getString("authtoken");
+        var username = rs.getString("username");
+        AuthTokenInformation authTokenInformation = new AuthTokenInformation(username);
+        authTokenInformation.setUsername(username);
+        authTokenInformation.setAuthToken(authtoken);
+        return authTokenInformation;
+    }
+
+    public void addAuth(AuthTokenInformation token) throws DataAccessException {
+        DatabaseManager databaseManager = new DatabaseManager();
+        var statement = "INSERT INTO auth (username, authtoken) VALUES (?, ?, ?)";
+        databaseManager.executeUpdate(statement, token.getUsername(), token.getAuthToken());
+    }
+// changing this file
+    public void removeAuth(String token) throws DataAccessException {
+        DatabaseManager databaseManager = new DatabaseManager();
+        var statement = "DELETE FROM auth username, authtoken WHERE authtoken=?";
+        // what happens if I try to delete something that is not there?
+        databaseManager.executeUpdate(statement, token);
+    }
+
+    public String getUsername(String token) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT username From auth WHERE authtoken= ?";
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.setString(1, token);
+                try (var rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return readUsername(rs);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException("Error: unable to read data");
+        }
+        return null;
+    }
+
+    private String readUsername(ResultSet rs) throws SQLException {
+        var username= rs.getString("username");
+        return username;
+    }
+
+    public String getAuth(String user) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT authtoken From auth WHERE username= ?";
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.setString(1, user);
+                try (var rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return readAuth(rs);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException("Error: unable to read data");
+        }
+        return null;
+    }
+
+    private String readAuth(ResultSet rs) throws SQLException {
+        var authtoken = rs.getString("authtoken");
+        return authtoken;
+    }
+
+    public void clear() throws DataAccessException {
+        DatabaseManager databaseManager = new DatabaseManager();
+        var statement = "TRUNCATE auth";
+        databaseManager.executeUpdate(statement);
+    }
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error: unable to configure database");
+        }
+    }
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS auth (
+            `username` varchar(255) NOT NULL,
+            `authtoken` varchar(255) NOT NULL,
+            PRIMARY KEY(`authtoken`, `username`)
+            );
+            """
+    };
 }
