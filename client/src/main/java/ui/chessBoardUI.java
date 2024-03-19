d353797fa88dd5b91ef42fa683f79db7e1978b15
creedthomp <ctt12@byu.edu>
2024-03-18 19:57:17 -0600

    Phase 5 push 5. actually done with printing the board now

--- client/src/main/java/ui/chessBoardUI.java
+++ client/src/main/java/ui/chessBoardUI.java
@@ -1,5 +1,9 @@
 package ui;
 
+import chess.ChessBoard;
+import chess.ChessGame;
+import chess.ChessPiece;
+
 import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
@@ -9,38 +13,79 @@ import java.util.Vector;
 
 import static ui.EscapeSequences.*;
 public class chessBoardUI {
-
+    private static boolean isBackwards = false;
     // i think I can change from main now that prints right
-    public static void main(String[][] pieces) {
+    public static void main(String[] args) {
+//        var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+//        out.print(ERASE_SCREEN);
+//
+//        ChessBoard pieces = new ChessBoard();
+//        pieces.resetBoard();
+//        setDarkerGray(out);
+//        printBorders(out, new String[]{" a ", " b ", " c ", " d ", " e ", " f ", " g ", " h "});
+//        drawBoard(out, new String[]{" 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 "}, pieces);
+//        printBorders(out, new String[]{" a ", " b ", " c ", " d ", " e ", " f ", " g ", " h "});
+//
+//        printBorders(out, new String[]{" h ", " g ", " f ", " e ", " d ", " c ", " b ", " a "});
+//        drawBoard(out, new String[]{" 8 ", " 7 ", " 6 ", " 5 ", " 4 ", " 3 ", " 2 ", " 1 "}, pieces);
+//        printBorders(out, new String[]{" h ", " g ", " f ", " e ", " d ", " c ", " b ", " a "});
+//
+//       out.print(SET_BG_COLOR_BLACK);
+//       out.print(SET_TEXT_COLOR_WHITE);
+
+        printBothBoards();
+    }
+
+    //I should make  ChessPiece[][] this a param eventually
+    public static void printBothBoards(){
         var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
         out.print(ERASE_SCREEN);
+        ChessBoard pieces = new ChessBoard();
+        pieces.resetBoard();
+        ChessPiece[][] board = pieces.getBoard();
 
         setDarkerGray(out);
         printBorders(out, new String[]{" a ", " b ", " c ", " d ", " e ", " f ", " g ", " h "});
-        drawBoard(out, new String[]{" 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 "}, pieces);
+        drawBoard(out, new String[]{" 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 "}, board);
         printBorders(out, new String[]{" a ", " b ", " c ", " d ", " e ", " f ", " g ", " h "});
-
+        out.println();
         printBorders(out, new String[]{" h ", " g ", " f ", " e ", " d ", " c ", " b ", " a "});
-        drawBoard(out, new String[]{" 8 ", " 7 ", " 6 ", " 5 ", " 4 ", " 3 ", " 2 ", " 1 "}, pieces);
+        isBackwards = true;
+        drawBoard(out, new String[]{" 8 ", " 7 ", " 6 ", " 5 ", " 4 ", " 3 ", " 2 ", " 1 "}, board);
         printBorders(out, new String[]{" h ", " g ", " f ", " e ", " d ", " c ", " b ", " a "});
-
-       out.print(SET_BG_COLOR_BLACK);
-       out.print(SET_TEXT_COLOR_WHITE);
+        isBackwards = false;
+        out.print(SET_BG_COLOR_BLACK);
+        out.print(SET_TEXT_COLOR_WHITE);
     }
-    private static void drawBoard(PrintStream out, String[] labelList, String[][] boardList) {
-        int counter = 0;
+    private static void drawBoard(PrintStream out, String[] labelList, ChessPiece[][] board) {
+        int counter;
+        if (isBackwards) {
+            counter = 7;
+        } else {
+            counter = 0;
+        }
         for (String label : labelList) {
             if (counter % 2 == 0) {
-                drawOneRow(out, SET_BG_COLOR_LIGHT_GREY, label, boardList[counter]);
+                drawOneRow(out, SET_BG_COLOR_LIGHT_GREY, label, board[counter]);
             }
             else {
-                drawOneRow(out, SET_BG_COLOR_DARK_GREY, label, boardList[counter]);
+                drawOneRow(out, SET_BG_COLOR_DARK_GREY, label, board[counter]);
             }
-            counter++;
+
+            if (isBackwards) {
+                counter--;
+            }
+            else {
+                counter++;
+            }
         }
     }
 
-    private static void drawOneRow(PrintStream out, String color, String label, String[] rowList) {
+
+    private static void drawOneRow(PrintStream out, String color, String label, ChessPiece[] rowList) {
+        if (isBackwards) {
+            rowList = reverseRow(rowList);
+        }
         for (int i = 0; i < 3; i++) {
             printSideHelp(out, i, label);
             for (int j = 0; j < 8; j++) { // i am going to switch like my whole implementaiton
@@ -58,7 +103,12 @@ public class chessBoardUI {
                     printTopBottomSquare(out);
                 }
                 else{
-                    printTheMiddleSquare(out, rowList[j]);
+                    if (rowList[j] == null) {
+                        printTheMiddleSquare(out, rowList[j], ChessGame.TeamColor.WHITE);
+                    } else {
+                        printTheMiddleSquare(out, rowList[j], rowList[j].getTeamColor());
+                    
+                    }
                 }
             }
             printSideHelp(out, i, label);
@@ -84,17 +134,38 @@ public class chessBoardUI {
     private static void printTopBottomSquare(PrintStream out) {
             //out.print(" ");
             out.print(EMPTY.repeat(3));
-
     }
 
-    private static void printTheMiddleSquare(PrintStream out, String piece) { // probably need to add a chess board as a param
+    private static void printTheMiddleSquare(PrintStream out, ChessPiece piece, ChessGame.TeamColor color) { // probably need to add a chess board as a param
+            String printPiece = "";
+            if (color == ChessGame.TeamColor.BLACK){
+                out.print(SET_TEXT_COLOR_BLACK);
+            }
+            else {
+                out.print(SET_TEXT_COLOR_WHITE);
+            }
             out.print(EMPTY);
-            out.print(" " + piece + " ");
+            if (piece != null) {
+                switch (piece.getPieceType()) {
+                    case KING -> printPiece = " K ";
+                    case QUEEN -> printPiece = " Q ";
+                    case BISHOP -> printPiece = " B ";
+                    case ROOK -> printPiece = " R ";
+                    case KNIGHT -> printPiece = " N ";
+                    case PAWN -> printPiece = " P ";
+                    default -> printPiece = "   ";
+                }
+            }
+            else {
+                printPiece = "   ";
+            }
+            out.print(printPiece);
             out.print(EMPTY);
     }
 
     private static void printBorders(PrintStream out, String[] list) {
         setDarkerGray(out);
+        out.print(SET_TEXT_COLOR_WHITE);
         out.print(EMPTY);
         for (int i = 0; i < 8; i++) {
             out.print(EMPTY);
@@ -107,6 +178,7 @@ public class chessBoardUI {
     }
 
     private static void printSideHelp(PrintStream out, int i, String label) {
+        out.print(SET_TEXT_COLOR_WHITE);
         if (i == 1) {
             setDarkerGray(out);
             out.print(label);
@@ -117,6 +189,16 @@ public class chessBoardUI {
         }
         setBlack(out);
     }
-
+    
+    private static ChessPiece[] reverseRow(ChessPiece[] rowList) {
+        ChessPiece[] backwards = new ChessPiece[8];
+        int counter = 0;
+        for (int i = 7; 0 <= i; i--) {
+            backwards[counter] = rowList[i];
+            counter++;
+        }
+        return backwards;
+    }
+        
 
 }
