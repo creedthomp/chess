8108e55165fff4ef0d770d75ba6b437c3bf4323e
creedthomp <ctt12@byu.edu>
2024-04-09 11:59:15 -0600

    Phase 6 push 8. forgot these

---
+++ server/src/main/java/server/WebSocket/Connection.java
@@ -0,0 +1,21 @@
+package server.WebSocket;
+
+import org.eclipse.jetty.websocket.api.Session;
+
+import java.io.IOException;
+
+public class Connection {
+    public String visitorName;
+    public Session session;
+    public int gameId;
+
+    public Connection(String visitorName, Session session, int gameId) {
+        this.visitorName = visitorName;
+        this.gameId = gameId;
+        this.session = session;
+    }
+
+    public void send(String msg) throws IOException {
+        session.getRemote().sendString(msg);
+    }
+}
